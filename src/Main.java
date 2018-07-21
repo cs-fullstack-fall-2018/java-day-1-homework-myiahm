cba470da8f2bbceb616ff12bdbea836c993080fa
Myiah Moore <myiahsade@gmail.com>
2018-07-21 10:06:21 -0500

    check in

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        int user = 0;
+        Scanner gitThe = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int userNumber = gitThe.nextInt();
+
+        if ((userNumber >= 1) && (userNumber <= 3)) {
+            System.out.print("correct:");
+
+        } else {
+            System.out.print("error!!! ");
+        }
+    }
+}
+
+
